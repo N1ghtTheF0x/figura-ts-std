24bc9d5ed2547e2e9a624781e7e191eb7a3f6aa7
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-12-14 17:35:44 +0100

    abstract avatar class

--- source/avatar.ts
+++ source/avatar.ts
@@ -1,45 +1,30 @@
-import { stringify } from "./string"
+import { registerHostOnlyEvent } from "./event"
+import ActionWheelPage from "./page"
+import { loadVariables } from "./vars"
 
-// all the keys of the avatar
-const __avatarKeys: Array<string> = []
-export class Avatar
+const AVATAR_LOCAL_ENTITY_INIT_EVENT_NAME = "figura-ts-std-avatar-init"
+
+export abstract class Avatar
 {
-    
+    public readonly actionWheel: ActionWheelPage = new ActionWheelPage("Main").use()
+    public load(): void
+    {
+        registerHostOnlyEvent("ENTITY_INIT",() => this.init(),AVATAR_LOCAL_ENTITY_INIT_EVENT_NAME)
+    }
+    public init(): void
+    {
+        vanilla_model.PLAYER.setVisible(false)
+        loadVariables()
+    }
 }
+
 export namespace Avatar
 {
-    /**
-     * Store avatar variable `value` as `key`. The Key can be a instance of any kind
-     * @param key A instance of any kind
-     * @param value A value
-     */
-    export function setVariable(key: any,value: any): void
+    export type Constructor<A extends Avatar> = new () => A
+    export function load<A extends Avatar>(Class: Constructor<A>): A
     {
-        const k = stringify(key)
-        if(!__avatarKeys.includes(k))
-            __avatarKeys.push(k)
-        avatar.store(tostring(__avatarKeys.indexOf(k)),value)
-    }
-    /**
-     * Retrieve the avatar variable at `key`, even if it's nil. The key can be a instance of any kind
-     * @param key A instance of any kind
-     * @param required Throw if the variable is nil
-     */
-    export function getVariable<V>(key: any,required?: false): V | undefined
-    /**
-     * Retrieve the avatar variable at `key`. The key can be a instance of any kind
-     * @param key A instance of any kind
-     * @param required Throw if the variable is nil
-     * @throws If avatar variable is nil
-     */
-    export function getVariable<V>(key: any,required?: true): V
-    export function getVariable<V>(key: any,required: boolean = true): V | undefined
-    {
-        const k = stringify(key)
-        const vars = world.avatarVars().get(avatar.getUUID()) ?? new LuaTable
-        const value = vars.get(tostring(__avatarKeys.indexOf(k)))
-        if(value === undefined && required)
-            error(`avatar variable '${key}' is nil`,1)
-        return value
+        const a = new Class()
+        a.load()
+        return a
     }
 }
