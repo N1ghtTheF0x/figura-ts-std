a403f1b006e441e4300a551e9165ba39fcb75a41
N1ghtTheF0x <kilo.night.prower@gmail.com>
2025-12-14 17:35:24 +0100

    type-safe way of getting events

--- source/event.ts
+++ source/event.ts
@@ -1,5 +1,9 @@
 import { hostDebug } from "./console"
 
+export function getFiguraLuaEvent<K extends keyof FiguraEventMap>(eventName: K): FiguraLuaEvent<FiguraEventMap[K]> | undefined
+{
+    return eventName in events ? events[eventName] as FiguraLuaEvent<FiguraEventMap[K]> : undefined
+}
 /**
  * Register a event only in a host environment. This won't get executed by remote players
  * @param eventName The event name
@@ -12,7 +16,7 @@ export function registerHostOnlyEvent<K extends keyof FiguraEventMap>(eventName:
     if(eventName in events)
     {
         hostDebug("Registered host-only event",name ?? "???","for",eventName);
-        (events[eventName] as FiguraLuaEvent<FiguraEventMap[K]>)
+        getFiguraLuaEvent(eventName)!
         .register(callback,name as string)
     }
 }
